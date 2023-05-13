a2d9f991dee745da4804f18b4d156e167c912a0f
GamerZero <WildlandHerald@mail.ru>
2023-05-13 08:55:34 +0300

    Add backend folder with hello world graphql query

---
+++ backend/src/test/kotlin/app/glsl/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.glsl.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

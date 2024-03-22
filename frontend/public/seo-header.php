204bfdef634b2695a93c66d642c96817ad38096d
Roman <22231294+Lutymane@users.noreply.github.com>
2024-03-22 13:10:23 +0500

    refactor(seo+sitemap): use sandbox instead of playground and add spanish meta

--- frontend/public/seo-header.php
+++ frontend/public/seo-header.php
@@ -2,20 +2,24 @@
 $lang = explode('-', $_GET['lang'] ?? 'en')[0];
 $localization = [
     'en' => [
-        "Online WebGL (GLSL) Shaders Editor and Playground",
-        "Modern Online WebGL (GLSL) Shaders Editor and Playground. Write shaders with ease thanks to advanced IntelliSense, autocompletion features, composability with shader libraries and a user-friendly interface for tweaking values and colors"
+        "Online WebGL (GLSL) Shaders Editor and Sandbox",
+        "Modern Online WebGL (GLSL) Shaders Editor and Sandbox. Write shaders with ease thanks to advanced IntelliSense, autocompletion features, composability with shader libraries and a user-friendly interface for tweaking values and colors"
     ],
     'ru' => [
         "Онлайн WebGL (GLSL) Редактор Шейдеров и Песочница",
         "Современный онлайн-редактор и песочница шейдеров WebGL (GLSL). Создавайте шейдеры легко благодаря продвинутой поддержке IntelliSense, функциям автодополнения, возможности компоновки с библиотеками шейдеров и удобному интерфейсу для настройки значений и цветов",
     ],
+    'es' => [
+        "Editor de Shaders y Salvadera de WebGL (GLSL) Online",
+        "Editor WebGL (GLSL) moderno de Shaders y Salvadera. Escribe shaders con facilidad gracias a inteligencia avanzada, características de autocompletado, componibilidad con bibliotecas de shaders y una interfaz fácil de usar para ajustar valores y colores."
+    ],
     'de' => [
-        "Online WebGL (GLSL) Shader-Editor und Spielplatz",
-        "Moderner Online WebGL (GLSL) Shader-Editor und Spielplatz. Einfaches Schreiben von Shadern dank fortschrittlichem IntelliSense, Autovervollständigung, Kompatibilität mit Shader-Bibliotheken und einer benutzerfreundlichen Oberfläche zum Anpassen von Werten und Farben",
+        "Online WebGL (GLSL) Shader-Editor und Sandkasten",
+        "Moderner Online WebGL (GLSL) Shader-Editor und Sandkasten. Einfaches Schreiben von Shadern dank fortschrittlichem IntelliSense, Autovervollständigung, Kompatibilität mit Shader-Bibliotheken und einer benutzerfreundlichen Oberfläche zum Anpassen von Werten und Farben",
     ],
     'zh' => [
         "在线 WebGL (GLSL) 着色器编辑器和沙箱",
-        "现代在线 WebGL (GLSL) 着色器编辑器和沙箱。 借助先进的 IntelliSense、自动完成、着色器库的可组合性以及用于调整值和颜色的用户友好界面，轻松编写着色器"
+        "现代在线 WebGL (GLSL) 着色器编辑器和沙箱。 借助先进的 IntelliSense、自动完成功能、着色器库的可组合性以及用于调整值和颜色的用户友好界面，可以轻松编写着色器"
     ]
 ];
 

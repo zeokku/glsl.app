c169d8b2d12296c1f4a881ee4dba67ae75794f75
Roman <22231294+Lutymane@users.noreply.github.com>
2025-03-02 19:40:17 +0500

    ref(Editor+Canvas+Options): improvements
    - Preserve canvas position during switching between narrow screen view and normal (movable directive)
    - Add "hide minimap" editor option (close #27)
    - Add formatting options

--- app/src/components/OptionsModal/Index.vue
+++ app/src/components/OptionsModal/Index.vue
@@ -4,14 +4,31 @@
     h1.App__font-shade {{ t("options") }}
     section.flex
       label
-        input(type="checkbox", v-model="offlineShareLink")
+        input(type="checkbox", v-model="settingValues.offlineShare")
         | {{ t("offline-link") }}
+    h2.App__font-shade {{ t("editor") }}
+    section.flex
+      label
+        input(type="checkbox", v-model="settingValues.editorMinimap")
+        | {{ t("show-minimap") }}
+    h2.App__font-shade {{ t("formatting") }}
+    section.flex(style="flex-flow: row")
+      label.cdn-input
+        .CExportModal_Index__label {{ t("print-width") }}
+        .App__input-wrap.App__glow-element-wrap
+          input.App__glow-element(type="number", v-model="settingValues.printWidth")
+      label.cdn-input
+        .CExportModal_Index__label {{ t("tab-size") }}
+        .App__input-wrap.App__glow-element-wrap
+          input.App__glow-element(type="number", v-model="settingValues.tabSize")
+    h2.App__font-shade {{ t("storage") }}
+    section.flex
       label.cdn-input
         .CExportModal_Index__label {{ t("npm") }}
         .App__input-wrap.App__glow-element-wrap
-          input.App__glow-element(v-model="npmCdn")
+          input.App__glow-element(v-model="settingValues.npmPackageProvider")
       label
-        input(type="checkbox", v-model="cachePackagesOffline")
+        input(type="checkbox", v-model="settingValues.cachePackages")
         | {{ t("cache") }}
       Button.clear-cache-btn(@click="clearCache")
         template(#icon) 
@@ -27,28 +44,49 @@
 import Button from "@/components/MenuBar/Button.vue";
 import Trash from "@/components/icons/trash.vue";
 
-import { getSetting, setSetting } from "@/settings";
-import { watch, onMounted } from "vue";
+import { getAllSettings, getSetting, setSettings } from "@/settings";
+import { watch, onMounted, shallowRef, reactive } from "vue";
 
 import { useMouse } from "@/composition/useMouse";
 import { updateElGlow } from "@/stylingUtils/updateGlow";
 
 import { useI18n } from "petite-vue-i18n";
 import { clearPersistentCache } from "@/includesOfflineCache";
+import { getEditor, getModel } from "../Editor.vue";
+import { useScreen } from "@/composition/useScreen";
 
 const { t, locale } = useI18n();
 
-let offlineShareLink = $shallowRef<boolean>(getSetting("offlineShare"));
-let npmCdn = $shallowRef<string>(getSetting("npmPackageProvider"));
-let cachePackagesOffline = $shallowRef<boolean>(getSetting("cachePackages"));
+const settingValues = reactive(getAllSettings());
+watch(settingValues, () => setSettings(settingValues));
 
-watch([$$(offlineShareLink), $$(npmCdn), $$(cachePackagesOffline)], () => {
-  setSetting({
-    offlineShare: offlineShareLink,
-    npmPackageProvider: npmCdn,
-    cachePackages: cachePackagesOffline,
-  });
-});
+const screen = useScreen();
+
+watch(
+  () => settingValues.editorMinimap,
+  v => {
+    const enabled = v && screen.w > 500;
+
+    getEditor().updateOptions({
+      minimap: {
+        enabled,
+      },
+    });
+  }
+);
+
+watch(
+  () => settingValues.tabSize,
+  s => {
+    console.log("tab update", s);
+
+    getEditor().updateOptions({
+      tabSize: s,
+    });
+
+    getModel().updateOptions({ tabSize: s });
+  }
+);
 
 const clearCache = () => {
   clearPersistentCache();

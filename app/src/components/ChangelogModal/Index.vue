7c447e05c265cb059c307b12924bb41e6d61e103
Roman <22231294+Lutymane@users.noreply.github.com>
2025-05-01 12:33:52 +0500

    feat: 1.5 changelog + markdown tweaks

--- app/src/components/ChangelogModal/Index.vue
+++ app/src/components/ChangelogModal/Index.vue
@@ -35,10 +35,14 @@ const onLinkClick = () => {
     align-items: center;
     gap: 0.75em;
 
-    margin-block: 1.5rem;
+    margin-block: 5rem 1.5rem;
+
+    &:first-of-type {
+      margin-top: 2rem;
+    }
 
     code {
-      color: #9a9a00;
+      color: hsl(323 60% 70% / 1);
       font-size: 0.75em;
 
       background: rgb(27 27 27 / 50%);
@@ -60,6 +64,36 @@ const onLinkClick = () => {
     margin-block: 4rem -2rem;
     font-size: 0.9rem;
     text-align: center;
+  }
+
+  // li {
+  //   text-wrap-style: balance;
+  // }
+
+  kbd {
+    display: inline-flex;
+    align-items: center;
+    gap: 0.2rem;
+
+    padding: 0.2rem 0.3rem;
+
+    color: white;
+    // @note content in top kbd will be muted typically + symbols
+    &:has(> kbd) {
+      color: hsl(0 7% 60% / 1);
+    }
+
+    > kbd {
+      padding-block: 0;
+    }
+
+    font-size: 0.9rem;
+
+    background: hsl(0, 7%, 18.34%);
+    border-radius: 0.5rem;
+
+    border: 1px solid var(--border);
+    border-bottom-width: 2px;
   }
 }
 </style>
